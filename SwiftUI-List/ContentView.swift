e86682ed64599bc88865a34246e654de7482e82f
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-11-03 10:15:10 -0500

    initial version

--- SwiftUI-List/ContentView.swift
+++ SwiftUI-List/ContentView.swift
@@ -1,16 +1,57 @@
-//
-//  ContentView.swift
-//  SwiftUI-List
-//
-//  Created by R. Mark Volkmann on 11/3/21.
-//
-
 import SwiftUI
 
-struct ContentView: View {
+struct Dog {
+    var name: String
+    var breed: String
+}
+
+let dogs = [
+    Dog(name: "Maisey", breed: "Treeing Walker Coonhound"),
+    Dog(name: "Ramsay", breed: "Native American Indian Dog"),
+    Dog(name: "Oscar", breed: "German Shorthaired Pointer"),
+    Dog(name: "Comet", breed: "Whippet")
+]
+
+struct DogRow: View {
+    var dog: Dog
+    var selected: Bool
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        // TODO: How can you control the column widths?
+        HStack {
+            Text(dog.name).foregroundColor(selected ? .green : .black)
+            Spacer()
+            Text(String(dog.name.count))
+            Spacer()
+            Text(dog.breed)
+        }
+    }
+}
+
+struct ContentView: View {
+    // @State private var selection: String? // single selection
+    @State private var selection = Set<String>() // multiple selection
+
+    func deleteDog() {
+        print("in deleteDog")
+    }
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                List(dogs, id: \.name, selection: $selection) { dog in
+                    DogRow(dog: dog, selected: selection.contains(dog.name))
+                }
+                // .onDelete(perform: deleteDog)
+                .listStyle(.grouped)
+                .toolbar {
+                    EditButton()
+                }
+                if !selection.isEmpty {
+                    Text("You selected \(selection.joined(separator: ", "))")
+                }
+            }
+        }
     }
 }
 
