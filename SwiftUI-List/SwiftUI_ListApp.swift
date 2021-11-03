60ac63f225f521a23dca2b56786e6915a121a791
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-11-03 09:24:56 -0500

    Initial Commit

---
+++ SwiftUI-List/SwiftUI_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ListApp.swift
+//  SwiftUI-List
+//
+//  Created by R. Mark Volkmann on 11/3/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
